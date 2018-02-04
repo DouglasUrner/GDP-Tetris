6a469712c097447b2eda0d2d54cf01e0ec8ba95d
Douglas Urner <dlu@canishe.com>
2018-02-04 14:36:21 -0800

    Implementing suggestions from Rider.

--- Tetris/Assets/Scripts/Spawner.cs
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -6,12 +6,12 @@ public class Spawner : MonoBehaviour {
 	public GameObject[] Tetrominos;
 
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 		SpawnNext();
 	}
 
 	public void SpawnNext() {
-		int i = Random.Range(0, Tetrominos.Length);
+		var i = Random.Range(0, Tetrominos.Length);
 		
 		// Spawn a Tetromino at the current location.
 		Instantiate(Tetrominos[i], transform);
