dbb3fd94a93ca44e56056129b4f91920b3f24f84
Douglas Urner <dlu@canishe.com>
2018-02-03 22:02:09 -0800

    Create Spawner script.

---
+++ Tetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+  // Tetrominos
+	public GameObject[] tetrominos;
+
+	// Use this for initialization
+	void Start () {
+		spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void spawnNext() {
+		int i = Random.Range(0, tetrominos.Length);
+		
+		// Spawn a Tetromino at the current location.
+		Instantiate(tetrominos[i], transform);
+	}
+}
